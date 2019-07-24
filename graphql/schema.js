8ece0c4e37607fa3fe26063805e39caf513d04af
Tucker McKinney <noemata83@gmail.com>
2019-07-23 20:44:22 -0400

    Refactor graphql schema to avoid circular references

--- graphql/schema.js
+++ graphql/schema.js
@@ -5,9 +5,131 @@ const {
   GraphQLSchema, 
   GraphQLList, 
   GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLBoolean
 } = graphql;
-const IngredientType = require('./Ingredient');
-const SpecType = require('./Spec');
+
+const { createIngredient, findIngredient } = require('../controllers/IngredientController');
+
+const MeasureEnumType = new GraphQLEnumType({
+  name: 'MeasureEnum',
+  values: {
+    OZ: {
+      value: 0,
+    },
+    ML: {
+      value: 1,
+    },
+    TSP: {
+      value: 2,
+    },
+    TBSP: {
+      value: 3,
+    },
+    DS: {
+      value: 4,
+    },
+    DR: {
+      value: 5,
+    },
+    PN: {
+      value: 6
+    },
+    BSP: {
+      value: 7,
+    },
+    SPL: {
+      value: 8,
+    },
+    RINSE: {
+      value: 9,
+    },
+    TWST: {
+      value: 10,
+    },
+    SPG: {
+      value: 11
+    },
+    SLI: {
+      value: 12
+    },
+    WDG: {
+      value: 13,
+    },
+    CUBE: {
+      value: 14,
+    }
+  }
+});
+
+const IngredientType = new GraphQLObjectType({
+	name: 'Ingredient',
+	fields: () => ({
+		id: {
+			type: GraphQLString
+		},
+		name: {
+			type: GraphQLString
+		},
+		type: {
+			type: GraphQLString
+		},
+		description: {
+			type: GraphQLString
+		},
+		spec: {
+			type: SpecType,
+			resolve(parentValue, args) {
+				return {}
+			}
+		}
+	})
+});
+
+
+const SpecIngredientType = new GraphQLObjectType({
+  name: 'SpecIngredient',
+  fields: () => ({
+    quantity: {
+      type: GraphQLFloat,
+    },
+    measure: {
+      type: MeasureEnumType
+    },
+    ingredient: {
+      type: IngredientType,
+    },
+    canSub: {
+      type: GraphQLBoolean
+    }
+  })
+});
+
+
+const SpecType = new GraphQLObjectType({
+  name: 'Spec',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    author: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    ingredients: {
+      type: new GraphQLList(SpecIngredientType)
+    },
+    riffOn: {
+      type: SpecType
+    },
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -15,14 +137,18 @@ const UserType = new GraphQLObjectType({
     id: {
       type: GraphQLString,
     },
+    username: {
+      type: GraphQLString,
+    },
     book: {
-      type: GraphQLList(SpecType)
+      type: new GraphQLList(SpecType)
     },
     shelf: {
-      type: GraphQLList(IngredientType)
+      type: new GraphQLList(IngredientType)
     }
   })
 });
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -40,10 +166,36 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return undefined
       }
+    },
+    ingredient: {
+      type: IngredientType,
+      args: { id: { type: GraphQLString },
+              name: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return findIngredient(args)
+      }
     }
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'mutation',
+  fields: {
+    addIngredient: {
+      type: IngredientType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString ) },
+        type: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLString },
+        spec: { type: GraphQLString }
+      },
+      resolve(parentValue, args ) {
+        return createIngredient(args);
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery,
+  query: RootQuery, mutation
 });
