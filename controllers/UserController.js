e9690a93c1417e27a187cb4a608450ee62416eb2
Tucker McKinney <noemata83@gmail.com>
2019-08-13 22:34:09 -0400

    Configured server to send cookies so that web client will accept them

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,113 +1,115 @@
-const User = require('../models/User');
-const Spec = require('../models/Spec');
-const Ingredient = require('../models/Ingredient').model;
-const jwt = require('jsonwebtoken');
-const config = require('../config/keys');
+const User = require("../models/User")
+const Spec = require("../models/Spec")
+const Ingredient = require("../models/Ingredient").model
+const jwt = require("jsonwebtoken")
+const config = require("../config/keys")
 
 const isDuplicate = (arr, id) => {
-  const found = arr.findIndex(item => item.id == id);
-  return found != -1;
+  const found = arr.findIndex(item => item.id == id)
+  return found != -1
 }
 
 const setTokenCookie = (res, token) => {
   res.cookie("appToken", token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60 * 24 * 7
-  });
+  })
 }
 
-const getUserById = async (id) => {
+const getUserById = async id => {
   try {
-    const user = await User.findById(id);
-    return user;
-  } catch(e) {
-    throw new Error('User not found.');
+    const user = await User.findById(id)
+    return user
+  } catch (e) {
+    throw new Error("User not found.")
   }
 }
 
 const getAllUsers = async () => {
-  const users = await User.find();
-  return users;
+  const users = await User.find()
+  return users
 }
 
-const deleteUser = async (id) => {
-  return await User.findByIdAndDelete(id);
+const deleteUser = async id => {
+  return await User.findByIdAndDelete(id)
 }
 
 const addIngredientToShelf = async (userId, ingredientId) => {
-  const user = await User.findById(userId);
-  if (isDuplicate(user.shelf, ingredientId)) throw new Error('Ingredient is already on your shelf.');
-  const ingredient = await Ingredient.findById(ingredientId);
-  if (!ingredient) throw new Error('Could not find ingredient.');
-  user.shelf.push(ingredient);
+  const user = await User.findById(userId)
+  if (isDuplicate(user.shelf, ingredientId))
+    throw new Error("Ingredient is already on your shelf.")
+  const ingredient = await Ingredient.findById(ingredientId)
+  if (!ingredient) throw new Error("Could not find ingredient.")
+  user.shelf.push(ingredient)
   try {
-    user.save();
-    return user;
+    user.save()
+    return user
   } catch (e) {
-    throw new Error('Error updating ingredient shelf.');
+    throw new Error("Error updating ingredient shelf.")
   }
-};
+}
 
 const removeIngredientFromShelf = async (userId, ingredientId) => {
-  const user = await User.findById(userId);
-  user.shelf = user.shelf.filter(ingredient => ingredient.id !== ingredientId);
+  const user = await User.findById(userId)
+  user.shelf = user.shelf.filter(ingredient => ingredient.id !== ingredientId)
   try {
-    user.save();
-    return user;
-  } catch(e) {
-    throw new Error('Error updating ingredient shelf.');
+    user.save()
+    return user
+  } catch (e) {
+    throw new Error("Error updating ingredient shelf.")
   }
 }
 
 const addSpecToBook = async (userId, specId) => {
-  const user = await User.findById(userId);
-  if (isDuplicate(user.book, specId)) throw new Error('Spec is already in your cocktail book.');
-  const spec = await Spec.findById(specId);
-  if (!spec) throw new Error('Could not find spec.');
-  user.book.push(spec);
+  const user = await User.findById(userId)
+  if (isDuplicate(user.book, specId))
+    throw new Error("Spec is already in your cocktail book.")
+  const spec = await Spec.findById(specId)
+  if (!spec) throw new Error("Could not find spec.")
+  user.book.push(spec)
   try {
-    user.save();
-    return user;
+    user.save()
+    return user
   } catch (e) {
-    throw new Error('Error updating cocktail book.');
-  }
-};
-
-const signUp = async (username, password, email, res) => {
-  const user = new User({ username, password, email, shelf: [], book: [] });
-  console.log(user);
-  try {
-    await user.save();
-    const payload = { username: user.username, id: user.id };
-    const options = { expiresIn: '3d' };
-    const secret = config.JWT_SECRET;
-    const token = jwt.sign(payload, secret, options);
-    setTokenCookie(res, token);
-    return {
-      ...user,
-      token
-    };
-  } catch(e) {
-    console.log(e);
+    throw new Error("Error updating cocktail book.")
   }
 }
 
-const login = async ( username, password, res ) => {
-  const user = await User.findOne({ username: username });
+const signUp = async (username, password, email, res) => {
+  const user = new User({ username, password, email, shelf: [], book: [] })
+  console.log(user)
+  try {
+    await user.save()
+    const payload = { username: user.username, id: user.id }
+    const options = { expiresIn: "3d" }
+    const secret = config.JWT_SECRET
+    const token = jwt.sign(payload, secret, options)
+    setTokenCookie(res, token)
+    return {
+      ...user,
+      token
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const login = async (username, password, res) => {
+  const user = await User.findOne({ username: username })
   if (user) {
-    const passwordIsValid = await user.isValidPassword(password);
+    const passwordIsValid = await user.isValidPassword(password)
     if (passwordIsValid) {
-      const payload = { username: user.username, id: user.id };
-      const options = { expiresIn: '3d' };
-      const secret = config.JWT_SECRET;
-      const token = jwt.sign(payload, secret, options);
-      setTokenCookie(res, token);
+      const payload = { username: user.username, id: user.id }
+      const options = { expiresIn: "3d" }
+      const secret = config.JWT_SECRET
+      const token = jwt.sign(payload, secret, options)
+      setTokenCookie(res, token)
       return {
         token
-      };
+      }
     } else {
-      throw new Error('Invalid password.');
+      throw new Error("Invalid password.")
     }
   }
 }
--- server.js
+++ server.js
@@ -1,34 +1,43 @@
-const express = require('express'),
-      app = express(),
-      expressGraphQL = require('express-graphql'),
-      mongoose = require('mongoose'),
-      schema = require('./graphql/schema'),
-      config = require('./config/keys'),
-      logger = require('./shared/logger'),
-      jwt = require('express-jwt'),
-      bodyParser = require('body-parser'),
-      cors = require('cors');
+const express = require("express"),
+  app = express(),
+  expressGraphQL = require("express-graphql"),
+  mongoose = require("mongoose"),
+  schema = require("./graphql/schema"),
+  config = require("./config/keys"),
+  logger = require("./shared/logger"),
+  jwt = require("express-jwt"),
+  bodyParser = require("body-parser"),
+  cors = require("cors")
 
-mongoose.Promise = global.Promise;
-mongoose.set('useFindAndModify', false);  
-mongoose.connect(config.mongoURI, { useNewUrlParser: true});
+const corsConfig = {
+  origin: config.frontendOrigin,
+  credentials: true
+}
+
+mongoose.Promise = global.Promise
+mongoose.set("useFindAndModify", false)
+mongoose.connect(config.mongoURI, { useNewUrlParser: true })
 
 const auth = jwt({
   secret: config.JWT_SECRET,
-  credentialsRequired: false,
-});
+  credentialsRequired: false
+})
 
-app.use(cors());
-
-app.use('/graphql', bodyParser.json(), auth, expressGraphQL((request, res) => ({
-  graphiql: true,
-  schema,
-  context: {
-    user: request.user,
-    res,
-  }})
-));
+app.use(
+  "/graphql",
+  cors(corsConfig),
+  bodyParser.json(),
+  auth,
+  expressGraphQL((request, res) => ({
+    graphiql: true,
+    schema,
+    context: {
+      user: request.user,
+      res
+    }
+  }))
+)
 
 app.listen(4000, () => {
-  logger.info('Bon Vivant Cocktail API running on port 4000');
+  logger.info("Bon Vivant Cocktail API running on port 4000")
 })
