57bf6e6d0b14a82be58d9d9904638beb1ac3ca7b
Tucker McKinney <noemata83@gmail.com>
2019-10-01 21:00:19 -0400

    Add test for constructQuery function, which identifies the target. TDD FTW.

---
+++ shared/constructQuery.test.js
@@ -0,0 +1,26 @@
+const constructQuery = require('./constructQuery')
+
+test('constructs a valid mongoose populate object from a GraphQL query', () => {
+  const graphQlQuery = {
+    ingredients: {
+      ingredient: {
+        name: {
+          $eq: 'Test Cocktail'
+        },
+        family: {
+          name: {
+            $eq: 'Amaro'
+          }
+        }
+      }
+    }
+  }
+  expect(constructQuery(graphQlQuery)).toEqual({
+    path: 'ingredients.ingredient',
+    match: { name: { $eq: 'Test Cocktail' } },
+    populate: {
+      path: 'family',
+      match: { name: { $eq: 'Amaro' } }
+    }
+  })
+})
